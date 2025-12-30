0be56a25452f2a047ddc803cb426f24ba0eb70c7
Flavius Ciocan <flaviusciocan@gmail.com>
2025-12-31 00:28:03 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "serif" }}>
+      <h1>ZARIA-Live</h1>
+      <p>Sistem activ. Bine ai venit.</p>
+    </div>
+  );
+}
