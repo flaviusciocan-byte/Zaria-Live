e5ca6f117d0c164df038403b1ab807292f7f6c57
Flavius Ciocan <flaviusciocan@gmail.com>
2025-12-31 09:07:59 +0200

    Update version.js

--- pages/api/zaria/version.js
+++ pages/api/zaria/version.js
@@ -1,6 +1,11 @@
 export default function handler(req, res) {
   res.status(200).json({
     system: "ZARIA-Live",
-    version: "1.0.0"
+    version: {
+      number: "1.0.0",
+      codename: "AURORA",
+      stable: true
+    },
+    timestamp: new Date().toISOString()
   });
 }
