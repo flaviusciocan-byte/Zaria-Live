251e01d5dfb82d05dd71d9cf572554632de2b41e
Flavius Ciocan <flaviusciocan@gmail.com>
2025-12-31 13:56:54 +0200

    Update index.js

--- pages/ui/manifest/index.js
+++ pages/ui/manifest/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import ZariaLayout from "../../../components/layouts/ZariaLayout";
 
 export default function ManifestHome() {
   const [manifest, setManifest] = useState(null);
