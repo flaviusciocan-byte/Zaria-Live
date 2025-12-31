60f19d76493532f0c22ddd23a8ec0f1f647788f3
Flavius Ciocan <flaviusciocan@gmail.com>
2025-12-31 09:25:09 +0200

    Update uptime.js

--- pages/api/zaria/uptime.js
+++ pages/api/zaria/uptime.js
@@ -1,7 +1,9 @@
 export default function handler(req, res) {
   res.status(200).json({
     system: "ZARIA-Live",
-    uptime_seconds: process.uptime(),
+    uptime: {
+      seconds: process.uptime()
+    },
     timestamp: new Date().toISOString()
   });
 }
